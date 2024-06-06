c7ad6363c08bf92a2c9a49d51384247557e11ac7
unknown <saadallahibraim17@gmail.com>
2024-06-06 22:42:24 +0100

    still on it

--- Js/mediaFunc.js
+++ Js/mediaFunc.js
@@ -4,14 +4,21 @@
 var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
-    navLinks.classList.add("show-menu");
+    // navLinks.classList.add("show-menu");
     navLinks.style.left = "0";
 }
 
 function hideMenu() {
-    navLinks.classList.remove("show-menu");
+    // navLinks.classList.remove("show-menu");
     navLinks.style.left = "-300px";
 }
+
+    var targetElement = document.getElementById("myTarget");
+    if (targetElement) {
+        targetElement.addEventListener("click", hideMenu);
+    }
+    
+    
 
 document.addEventListener('click', function(event) {
     var isClickInsideNav = navLinks.contains(event.target);
@@ -21,27 +28,6 @@ document.addEventListener('click', function(event) {
         hideMenu();
     }
 });
-
-var hideMenuButton = document.getElementById("hideMenu");
-if (hideMenuButton) {
-    hideMenuButton.addEventListener("click", hideMenu);
-}
-
-document.addEventListener('DOMContentLoaded',
-    function () {
-        const navItems = document
-            .querySelectorAll('.nav-item');
- 
-        navItems.forEach(item => {
-            item.addEventListener('click',
-                function () {
-                    navItems.forEach(navItem => navItem
-                        .classList.remove('active'));
-                    this.classList.add('active');
-                });
-        });
-    });
-
 
 
     const header = document.getElementById("landing-page");
