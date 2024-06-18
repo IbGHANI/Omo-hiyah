4b7cffd323132be8f5bf763b0f0d171dc55f9598
unknown <saadallahibraim17@gmail.com>
2024-06-18 18:22:42 +0100

    ...!

--- Js/mediaFunc.js
+++ Js/mediaFunc.js
@@ -1,33 +1,29 @@
 
 
-// navbar 
+
 var navLinks = document.getElementById("navLinks");
-
-function showMenu() {
-    // navLinks.classList.add("show-menu");
-    navLinks.style.left = "0";
-}
-
-function hideMenu() {
-    // navLinks.classList.remove("show-menu");
-    navLinks.style.left = "-300px";
-}
+    
+    function showMenu(){
+        navLinks.style.left = "0";
+    }
+    function hideMenu(){
+        navLinks.style.left = "-300px";
+    }
 
     var targetElement = document.getElementById("myTarget");
     if (targetElement) {
         targetElement.addEventListener("click", hideMenu);
     }
-    
-    
 
-document.addEventListener('click', function(event) {
-    var isClickInsideNav = navLinks.contains(event.target);
-    var isClickOnShowMenuButton = document.getElementById("showMenu").contains(event.target);
 
-    if (!isClickInsideNav && !isClickOnShowMenuButton) {
-        hideMenu();
-    }
-});
+
+
+
+
+
+
+
+
 
 
     const header = document.getElementById("landing-page");
