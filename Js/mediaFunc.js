506d2ddac8cd149e2312b8d849e3a9114a01543b
unknown <saadallahibraim17@gmail.com>
2024-06-19 10:04:28 +0100

    still on it

--- Js/mediaFunc.js
+++ Js/mediaFunc.js
@@ -2,30 +2,61 @@
 
 
 var navLinks = document.getElementById("navLinks");
-    
-    function showMenu(){
-        navLinks.style.left = "0";
-    }
-    function hideMenu(){
-        navLinks.style.left = "-300px";
-    }
 
-    var targetElement = document.getElementById("myTarget");
-    if (targetElement) {
-        targetElement.addEventListener("click", hideMenu);
-    }
+function showMenu() {
+    navLinks.style.left = "0";
+    navLinks.classList.add("active"); // Add the "active" class to handle the display
+}
 
+function hideMenu() {
+    navLinks.style.left = "-300px";
+    navLinks.classList.remove("active"); // Remove the "active" class
+}
 
-
-
-
+var targetElement = document.getElementById("myTarget");
+        if (targetElement) {
+            targetElement.addEventListener("click", hideMenu);
+        }
 
 
 
+        // document.addEventListener('click', function(event) {
+        //         var isClickInsideNav = navLinks.contains(event.target);
+        //         var isClickOnShowMenuButton = document.getElementById("showMenu").contains(event.target);
+                        
+        //             if (!isClickInsideNav && !isClickOnShowMenuButton) {
+        //                 hideMenu();
+        //             }
+        //         });  
+
+// var navLinks = document.getElementById("navLinks");
+
+// function showMenu() {
+//     navLinks.style.left = "0";
+//     navLinks.classList.add("active"); // Add the "active" class to handle the display
+// }
+
+// function hideMenu() {
+//     navLinks.style.left = "-300px";
+//     navLinks.classList.remove("active"); // Remove the "active" class
+// }
 
 
+// document.addEventListener('click', function(event) {
+//     var isClickInsideNav = navLinks.contains(event.target);
+//     var isClickOnShowMenuButton = document.getElementById("showMenu").contains(event.target);
+            
+//         if (!isClickInsideNav && !isClickOnShowMenuButton) {
+//             hideMenu();
+//         }
+//     });  
 
 
+// var targetElement = document.getElementById("yourTarget");
+//         if (targetElement) {
+//             targetElement.addEventListener("click", hideMenu);
+//         }
+        
     const header = document.getElementById("landing-page");
 
     window.onscroll = function(){
